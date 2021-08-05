a1a751cc05b8ae37aa4950d9d95b138c8b7dafc1
Etzion Harari <etzionhar@gmail.com>
2021-08-05 11:56:21 +0300

    Add files via upload

---
+++ implementation.py
@@ -0,0 +1,18 @@
+from ch import ConvexH
+import numpy as np
+
+# example 1:
+points = np.load(r'examples\example_1.npy')
+ConvexH(points).plot(file=r'outputs\plot1_noraml',rnd=1)
+
+# example 2:
+points = np.load(r'examples\example_2.npy')
+ConvexH(points).plot(file=r'outputs\plot1_circle',rnd=1)
+
+# example 3:
+points = np.load(r'examples\example_3.npy')
+ConvexH(points).plot(file=r'outputs\plot1_square',rnd=1)
+
+# example 4:
+points = np.load(r'examples\example_4.npy')
+ConvexH(points).plot(file=r'outputs\plot1_hex',rnd=1)
